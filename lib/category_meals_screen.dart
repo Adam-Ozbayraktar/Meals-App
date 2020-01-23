4c3479a74e374fca31d40ea901519e21d6bbfc82
AdamOz <adamozbayraktar@live.com>
2020-01-23 17:26:22 +0200

    Added filter on receipies

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/models/category.dart';
+
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  static const routeName  = '/category-meals';
+  static const routeName = '/category-meals';
   // final String categoryId;
   // final String categoryTitle;
 
@@ -14,13 +15,19 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: ListView.builder(itemBuilder: (ctx, index){
-
-      }, itemCount: ,),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
+      ),
     );
   }
 }
